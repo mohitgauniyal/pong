e93e401ff82a145725a4d92b2736d0628b6b1d09
Mohit Gauniyal <mohitgauniyal19a@gmail.com>
2019-11-10 20:55:23 +0530

    Add score text on top

--- pong.py
+++ pong.py
@@ -39,6 +39,16 @@ ball.dx = 0.4
 ball.dy = 0.4
 
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A : 0    Player B : 0", align="center", font=("Courier", 22, "normal"))
+
+
 # Paddle moving functions
 def paddle_a_up():
     y = paddle_a.ycor()
